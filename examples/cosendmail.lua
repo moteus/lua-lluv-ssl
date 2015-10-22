cfadaa7d1719a46e1a570fff27ec79cb5629944d
Alexey Melnichuk <mimir@newmail.ru>
2015-10-22 18:53:55 +0300

    Update sendmail example.

--- examples/cosendmail.lua
+++ examples/cosendmail.lua
@@ -4,18 +4,13 @@ local ssl       = require "lluv.ssl"
 local socket    = require "lluv.ssl.luasocket"
 local sendmail  = require "sendmail"
 
-local Context = ssl.context{
-  -- ...
-}
-
-local ssl_create = function() return socket.ssl(Context) end
-
 ut.corun(function() print(sendmail{
   server = {
     address  = "localhost"; port = 465;
     user     = "moteus@test.localhost.com";
     password = "123456";
-    create   = ssl_create;
+    ssl      = ssl.context{...};
+    create   = socket.ssl;
   },
 
   from = {
